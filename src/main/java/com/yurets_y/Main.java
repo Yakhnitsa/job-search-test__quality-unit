e0d0b6d554a8437978a7ca947d3e98e18de7cd21
Yakhnitsa <transpmailbox@gmail.com>
2020-08-09 17:31:54 +0300

    Created the logic of the main class

--- src/main/java/com/yurets_y/Main.java
+++ src/main/java/com/yurets_y/Main.java
@@ -1,8 +1,38 @@
 package com.yurets_y;
 
+import com.yurets_y.service.*;
+import com.yurets_y.storage.StorageService;
+import com.yurets_y.storage.StorageServiceImpl;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        if(args.length == 0) return;
+        if(args.length == 1){
+            splitAndExecute(args[0]);
+        }else{
+            execute(args);
+        }
+    }
+
+    private static void splitAndExecute(String arg){
+        String lineSeparator = System.lineSeparator();
+        String[] args = arg.split(lineSeparator);
+        execute(args);
+    }
+    private static void execute(String[] args){
+        StorageService storageService = new StorageServiceImpl();
+        QueryExtractor queryExtractor = new QueryExtractorImpl();
+        WaitingTimelineExtractor waitingTimelineExtractor = new WaitingTimelineExtractorImpl();
+
+        MainExecutor mainExecutor = new MainExecutorImpl(queryExtractor,waitingTimelineExtractor,storageService);
+
+        StringBuilder sb = new StringBuilder();
+        Arrays.asList(args).forEach(line ->{
+            sb.append(mainExecutor.executeQuery(line));
+        });
+        System.out.println(sb);
     }
 }
